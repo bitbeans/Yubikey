efe17a486b7e4502eb1be0601880a505b3088126
Quezler <Quezler@me.com>
2016-04-01 10:05:13 +0200

    Removed 2 unnecessary spaces
    2 of the 5 yubico api urls have a space after the comma, it will show up in some editors.

--- src/config/yubikey.php
+++ src/config/yubikey.php
@@ -10,8 +10,8 @@ return array(
 
 	'URL_LIST' => array(
 						'api.yubico.com/wsapi/2.0/verify',
-						'api2.yubico.com/wsapi/2.0/verify', 
-						'api3.yubico.com/wsapi/2.0/verify', 
+						'api2.yubico.com/wsapi/2.0/verify',
+						'api3.yubico.com/wsapi/2.0/verify',
 						'api4.yubico.com/wsapi/2.0/verify',
 						'api5.yubico.com/wsapi/2.0/verify',
 						),
