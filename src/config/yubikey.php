29e148d816bd50e6a10a2cf9618aed5060309dd8
Ely Haughie <Elycin2@gmail.com>
2017-12-12 03:37:39 -0500

    Optimizing yubikey library. More ternary operator reliance over lengthy if statements array() to [] conversion. updated phpdocs to include @throws. simplified one line conditionals.

--- src/config/yubikey.php
+++ src/config/yubikey.php
@@ -1,21 +1,19 @@
 <?php
 
-// https://upgrade.yubico.com/getapikey/
+/*
+ * You will need an API Key from the official Yubikey Website
+ * https://upgrade.yubico.com/getapikey/
+ * */
 
-return array(
-
-	'CLIENT_ID' => '',
-
-	'SECRET_KEY' => '',
-
-	'URL_LIST' => array(
-						'api.yubico.com/wsapi/2.0/verify',
-						'api2.yubico.com/wsapi/2.0/verify',
-						'api3.yubico.com/wsapi/2.0/verify',
-						'api4.yubico.com/wsapi/2.0/verify',
-						'api5.yubico.com/wsapi/2.0/verify',
-						),
-
-	'USER_AGENT' => 'Laravel 5',
-
-);
+return [
+    'CLIENT_ID' => '',
+    'SECRET_KEY' => '',
+    'URL_LIST' => [
+        'api.yubico.com/wsapi/2.0/verify',
+        'api2.yubico.com/wsapi/2.0/verify',
+        'api3.yubico.com/wsapi/2.0/verify',
+        'api4.yubico.com/wsapi/2.0/verify',
+        'api5.yubico.com/wsapi/2.0/verify',
+    ],
+    'USER_AGENT' => 'Laravel 5',
+];
