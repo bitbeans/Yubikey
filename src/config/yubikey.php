0ca0ca328128fe106eb28ba552cb7ca3bd928743
Léopold Jacquot <leopold.jacquot@infomaniak.com>
2019-02-05 09:18:51 +0100

    Add HTTPS_VERIFY option to the configuration file

--- src/config/yubikey.php
+++ src/config/yubikey.php
@@ -6,14 +6,15 @@
  * */
 
 return [
-    'CLIENT_ID' => '',
-    'SECRET_KEY' => '',
-    'URL_LIST' => [
+    'CLIENT_ID'    => '',
+    'SECRET_KEY'   => '',
+    'URL_LIST'     => [
         'api.yubico.com/wsapi/2.0/verify',
         'api2.yubico.com/wsapi/2.0/verify',
         'api3.yubico.com/wsapi/2.0/verify',
         'api4.yubico.com/wsapi/2.0/verify',
         'api5.yubico.com/wsapi/2.0/verify',
     ],
-    'USER_AGENT' => 'Laravel 5',
+    'USER_AGENT'   => 'Laravel 5',
+    'HTTPS_VERIFY' => false,
 ];
